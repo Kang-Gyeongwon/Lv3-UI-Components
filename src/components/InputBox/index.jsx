f33bc443a288aac0ed28f13a19cfae3658673d03
Kang GyeongWon <88028208+Kang-Gyeongwon@users.noreply.github.com>
2023-07-06 15:59:15 +0900

    feat: form 데이터 버튼 누를때 내용 alert에 출력하기

--- src/components/InputBox/index.jsx
+++ src/components/InputBox/index.jsx
@@ -1,20 +1,50 @@
+import { useState } from "react";
 import { Title, Form, InputlLabel, Inputs, SaveBtn } from "./styled";
 
 const InputBox = () => {
+  const [formValue, setFormValue] = useState({
+    title: "",
+    price: 0,
+  });
+  const onInputData = (e) => {
+    setFormValue({
+      ...formValue,
+      [e.target.name]: e.target.value,
+    });
+  };
 
+  const onAdd = () => {
+    formValue.title === null || formValue.price === null
+      ? window.alert("이름과 가격 모두 입력해주세요.")
+      : window.alert(`name: ${formValue.title}, price: ${formValue.price}`);
+    setFormValue({
+      title: "",
+      price: 0,
+    });
+  };
   return (
     <>
       <Title>Input</Title>
       <Form>
         <div>
           <InputlLabel>이름</InputlLabel>
-          <Inputs type="text" />
+          <Inputs
+            onChange={onInputData}
+            name="title"
+            value={formValue.title}
+            type="text"
+          />
         </div>
         <div>
           <InputlLabel>가격</InputlLabel>
-          <Inputs type="text" />
+          <Inputs
+            onChange={onInputData}
+            name="price"
+            value={formValue.price}
+            type="text"
+          />
         </div>
-        <SaveBtn type="submit" value="저장" />
+        <SaveBtn onClick={onAdd} type="submit" value="저장" />
       </Form>
     </>
   );
