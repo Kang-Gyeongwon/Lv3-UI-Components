a705eee67bbc1245edb03ac851753d08ef1052f1
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-10 12:21:28 +0900

    refactor: App.jsx SelectCollection 컴포넌트 추가 및 불필요 코드 정리

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,17 @@ import GlobalStyle from "./styles/GlobalStyle";
 import Button from "./components/Shared/Button";
 import InputBox from "./components/InputBox";
 import ButtonBox from "./components/ButtonBox";
+import SelectCollection from "./components/SelectCollection"
+import ButtonCollection from "./components/ButtonCollection";
 
 function App() {
-  // ! 버튼 시연을 위한 임시 CSS, 이후 제거 필요
-  const tempStyle = {
-    margin : "1rem", 
-    display: "flex", 
-  }
     return (
         <>
             <GlobalStyle />
-            <div style={tempStyle}>
-              <Button onClick={()=>{}} negative={true} icon="notification" isIconRight={true}>Large Button with Icon (right)</Button>
-              <Button size="medium">Medium Button</Button>
-              <Button size="small">Small Button</Button>
-            </div>
-            <div style={tempStyle}>
-              <Button color="danger" onClick={()=>{}} negative={true} icon="checkbox" isIconRight={false}>Large Button with Icon (left)</Button>
-              <Button color="danger" size="medium" >Medium Button</Button>
-              <Button color="danger" size="small">Small Button</Button>
-            </div>
+            <ButtonCollection/>
             <ButtonBox />
             <InputBox />
+            <SelectCollection/>
         </>
     );
 }
