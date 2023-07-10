5d6de53b4711cd687c3413763888c54a72cee382
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-10 17:11:46 +0900

    refactor: 변경된 컴포넌트명 적용

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import GlobalStyle from "./styles/GlobalStyle";
 import InputBox from "./components/InputBox";
 import SelectCollection from "./components/SelectCollection"
 import ButtonCollection from "./components/ButtonCollection";
-import SelectCollection from "./components/SelectCollection";
 
 function App() {
     return (
