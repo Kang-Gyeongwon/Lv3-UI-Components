e864878a6ecfc62b7f18ed5a894b765821c73ccb
Seung Hyun Tark <makepin2r@gmail.com>
2023-07-10 12:22:36 +0900

    feat: ButtonCollection 컴포넌트 생성 (App 시연용)

---
+++ src/components/ButtonCollection/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    font-size: 30px;
+    font-weight: 700;
+    margin: 1rem;
+`;
+
+export const ButtonsWrapper = styled.div`
+    display: flex;
+    gap: 20px;
+    margin: 1rem;
+`;
